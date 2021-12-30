b59852c33e6f1e36fdf398a1859a455dc26eb430
Oskar Jiang <oskar.jiang@polestar.com>
2021-12-30 16:09:51 +0100

    Add log

--- src/handlers/addPlayer.ts
+++ src/handlers/addPlayer.ts
@@ -3,12 +3,14 @@ import { PlayersStore } from '../store'
 
 export const AddPlayer = (payload: string) => {
   console.log('*** Received payload')
+  console.log(payload)
   try {
     const player: IPlayer = JSON.parse(payload)
     PlayersStore.push(player)
     return new Response(null, { status: 200 })
   } catch (error) {
     console.log('*** Failed: error')
+    console.log(error)
     return new Response(null, { status: 400 })
   }
 }
