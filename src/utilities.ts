5476fd4b99efb3be73ea75d64fab82e8630bd1e0
Oskar Jiang <oskar.jiang@polestar.com>
2021-12-30 15:35:06 +0100

    Add cors header

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,8 @@
 export const PayloadToJsonResponse = (payload: any): Response => {
   const body = JSON.stringify(payload)
-  const headers = { 'Content-type': 'application/json' }
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-type': 'application/json',
+  }
   return new Response(body, { headers })
 }
