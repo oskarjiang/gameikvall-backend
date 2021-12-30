1230c7579e806f2415e8cf4d6a8f5ccca9bbadf9
Oskar Jiang <oskar.jiang@polestar.com>
2021-12-30 16:08:12 +0100

    Add errorhandling

--- src/handlers/addPlayer.ts
+++ src/handlers/addPlayer.ts
@@ -1,7 +1,14 @@
 import { IPlayer } from '../domain/player'
 import { PlayersStore } from '../store'
 
-export const AddPlayer = (player: IPlayer) => {
-  PlayersStore.push(player)
-  return new Response(null, { status: 200 })
+export const AddPlayer = (payload: string) => {
+  console.log('*** Received payload')
+  try {
+    const player: IPlayer = JSON.parse(payload)
+    PlayersStore.push(player)
+    return new Response(null, { status: 200 })
+  } catch (error) {
+    console.log('*** Failed: error')
+    return new Response(null, { status: 400 })
+  }
 }
